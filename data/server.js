5f738890788b29f3e6f828fc357b6aab565fd65c
MAllen07 <mrsmallen07@gmail.com>
2019-05-16 17:26:56 -0400

    made changes in server file

--- data/server.js
+++ data/server.js
@@ -1,7 +1,7 @@
 const express = require("express"); //commonJS syntax
 //equivalent to import express from 'express'
 
-const db = requires("./data/hubs-model.js");
+const db = require("./hubs-model.js");
 
 const server = express(); //this creates http web server
 
@@ -11,69 +11,68 @@ const server = express(); //this creates http web server
 //Explain what 'req' and 'res' are.
 
 server.get("/now", (req, res) => {
-    // name is not important (could be request, respond) position is
+  // name is not important (could be request, respond) position is
 
-    const now = new Date().toISOString();
-    res.send(now);
+  const now = new Date().toISOString();
+  res.send(now);
 });
 
 server.get("/hubs", (req, res) => {
-    db.find()
-        .then(hubs => res.status(200).json(hubs))
-        .catch(err => res.status(500).json({ success: false, err }));
+  db.find()
+    .then(hubs => res.status(200).json(hubs))
+    .catch(err => res.status(500).json({ success: false, err }));
 }); //must define route first
 
 //CRUD = create update delete
 
 server.post("/hubs", (req, res) => {
-    // one way a client can send information is in the request body
-    const hubInfo = req.body;
+  // one way a client can send information is in the request body
+  const hubInfo = req.body;
 
-    console.log({ hubInfo });
+  console.log({ hubInfo });
 
-    db.add(hubInfo)
-        .then(hub => res.status(201).json(hub))
-        .catch(err => res.status(500).json({ success: false, err }));
+  db.add(hubInfo)
+    .then(hub => res.status(201).json(hub))
+    .catch(err => res.status(500).json({ success: false, err }));
 });
 
 //DELETE
 server.delete("/hubs/:id", (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    db.remove(id)
-        .then(deleted => {
-            if (deleted) {
-                res.status(204).end();
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: "I can not find the hub you are looking for"
-                });
-            }
-        })
-        .catch();
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "I can not find the hub you are looking for"
+        });
+      }
+    })
+    .catch();
 });
 
 // UPDATE
 
 server.put("/hubs/:id", (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
+  const { id } = req.params;
+  const changes = req.body;
 
-    db.update(id, changes)
-        .then(updated => {
-            if (updated) {
-                res.status(200).json({ success: true, updated });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: "I cannot find the hub you are looking for"
-                });
-            }
-        })
-        .catch(err => res.status(500).json({ success: false, err }));
+  db.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json({ success: true, updated });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "I cannot find the hub you are looking for"
+        });
+      }
+    })
+    .catch(err => res.status(500).json({ success: false, err }));
 });
-
 
 // post - for create endpoint
 
@@ -82,8 +81,5 @@ server.put("/hubs/:id", (req, res) => {
 // );
 
 //listening port
-
-
-
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./api/server.js");
+const server = require("./data/server.js");
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log("\n*** Server Running on http://localhost:${port} ***\n");
